527be1a1219654cd9d62e2126f9c4e5de5a18114
quincshe <quincshe@student.21-school.ru>
2025-04-17 16:27:15 +0700

    view with model and @RequestParam

--- src/main/java/ru/didenko/spring/mvc/MyController.java
+++ src/main/java/ru/didenko/spring/mvc/MyController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -24,10 +25,17 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. " + empName;
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
+        empName = "Mr. " + empName + "!";
         model.addAttribute("nameAttribute", empName);
         return "show-emp-details-view";
     }
